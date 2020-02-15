3cb9f97075ded084d38ea7a2aaf3a5e240f2427a
mkevinq <33074023+mkevinq@users.noreply.github.com>
2020-02-15 04:44:18 -0500

    add some working api implementations and other functions

---
+++ util.py
@@ -0,0 +1,63 @@
+import urllib.request
+import urllib.error
+import base64
+import json
+from google.cloud import vision
+from google.cloud.vision import types
+import io
+import boto3
+
+def get_request(url, h):
+    request = urllib.request.Request(url, headers=h, method="GET")
+    with urllib.request.urlopen(request) as response:
+        data = response.read().decode('ascii')
+    return data
+
+def post_request(url, h ,b):
+    body = urllib.parse.urlencode(b).encode('ascii')
+    request = urllib.request.Request(url, data=body, headers=h, method="POST")
+    with urllib.request.urlopen(request) as response:
+        data = json.loads(response.read().decode('ascii'))
+    return data
+
+def read_face_gcv(image_path):
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(image_path, "rb") as image:
+        data = image.read()
+    
+    image = types.Image(content=data)
+    response = client.face_detection(image)
+
+    faces = response.face_annotations
+
+    face = faces[0]
+
+    emotions = [
+        {
+            'Type': 'JOY',
+            'Likelihood': face.joy_likelihood
+        }, 
+        {
+            'Type': 'SORROW',
+            'Likelihood': face.sorrow_likelihood
+        }, 
+        {
+            'Type': 'ANGER',
+            'Likelihood': face.anger_likelihood
+        }, 
+        {
+            'Type': 'SURPRISE',
+            'Likelihood': face.surprise_likelihood
+        }
+    ]
+
+    return emotions
+
+def read_face_aws(image_path):
+    client = boto3.client('rekognition')
+
+    with open(image_path, "rb") as image:
+        response = client.detect_faces(Image={'Bytes': image.read()}, Attributes=['ALL'])
+    
+    return response['FaceDetails'][0]['Emotions']
